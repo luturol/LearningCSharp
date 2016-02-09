a1f6cbb783dfbb9fa5297499d30c61660b0544ee
luturol <rafael.ahrons@gmail.com>
2016-02-09 18:48:51 -0200

    jogo da velha

---
+++ Jogo da Velha/Jogo da Velha/Jogador.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jogo_da_Velha
+{
+    class Jogador
+    {
+        private string nome;
+        private int vitorias;
+
+        public Jogador(string nome)
+        {
+            vitorias = 0;
+            this.nome = nome;
+        }
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public int getVitorias()
+        {
+            return vitorias;
+        }
+
+        public string toString()
+        {
+            string msg = "";
+            msg += "Nome: " + getNome();
+            msg += "\nVitorias: " + getVitorias();
+            return msg;
+        }
+    }
+}
---
+++ Jogo da Velha/Jogo da Velha/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jogo_da_Velha
+{
+    class Program
+    {
+        static ConsoleKeyInfo cki = new ConsoleKeyInfo(); //Global
+
+        static void Main(string[] args)
+        {
+            
+            ConfigurarJanela();
+            ConfigurarCorDaFonte(ConsoleColor.Green);
+            
+            menu();
+            
+        }
+        static void ConfigurarJanela()
+        {
+            Console.Title = "Jogo da Velha - By Rafael Ahrons";
+            Console.BufferHeight = 40;
+            Console.BufferWidth = 100;
+            Console.SetWindowSize(100, 40);
+        }
+
+        static void ConfigurarCorDaFonte(ConsoleColor corDaFonte)
+        {
+            Console.ForegroundColor = corDaFonte;
+        }
+
+        static void menu()
+        {
+            //TODO: Jogo só acaba se um usuário quiser encerrar a partida            
+            Tabuleiro tabuleiro = new Tabuleiro();
+            ModoDeJogo modoDeJogo;
+            do
+            {                
+                modoDeJogo = new ModoDeJogo();
+                
+            } while (!modoDeJogo.acabou);
+        }
+    }
+
+}
