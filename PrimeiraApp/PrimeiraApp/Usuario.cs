77186c821b1538dcab5e8295b1b288616ba39b61
luturol <rafael.ahrons@gmail.com>
2016-02-09 19:12:07 -0200

    Primeira app

---
+++ PrimeiraApp/PrimeiraApp/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimeiraApp
+{
+    class Usuario
+    {
+         
+        public String nome { get; set; }
+
+        private String sobrenome { get { return nome; } set { this.nome = nome; } }
+
+        public Usuario(String nome, String sobrenome)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+        }
+    }
+}
