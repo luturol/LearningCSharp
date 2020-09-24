426e6c3c3beea24390bd04b5993949e8edcd2bf6
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-09-24 15:11:39 -0300

    Add paddle movement with mouse, lose collider, ball stick to paddle

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle1;
+
+    Vector2 paddleToBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBall = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LockBallToPaddle();
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePosition + paddleToBall;
+    }
+}
---
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+}
