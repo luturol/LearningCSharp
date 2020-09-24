fabea4b3d652ad9b6a316cbf1019e5bb6ca2b9eb
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-09-24 16:43:36 -0300

    Add invisible walls

--- Block Breaker/Assets/Scripts/Ball.cs
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddle1;
-
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
     Vector2 paddleToBall;
+    private bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +19,26 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        LockBallToPaddle();
+        if(!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+        
     }
 
     private void LockBallToPaddle()
     {
         Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePosition + paddleToBall;
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
     }
 }
