5251bbd6b77d6f1a34c2414c847e649a071f2713
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-09-26 23:26:14 -0300

    add sound to ball

--- Block Breaker/Assets/Scripts/Ball.cs
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -41,4 +41,17 @@ public class Ball : MonoBehaviour
             GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
         }
     }
+    
+    /// <summary>
+    /// Sent when an incoming collider makes contact with this object's
+    /// collider (2D physics only).
+    /// </summary>
+    /// <param name="other">The Collision2D data associated with this collision.</param>
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(hasStarted)
+        {
+            GetComponent<AudioSource>().Play();    
+        }
+    }
 }
