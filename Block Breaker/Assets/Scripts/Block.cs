198d8551bc9027519ceb6b705343ac9f15f92e69
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-11-16 11:58:06 -0300

    Fixed array index bug and add smarter max life

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -7,7 +7,6 @@ public class Block : MonoBehaviour
     //config params
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
-    [SerializeField] int maxHits;
     [SerializeField] Sprite[] hitSprites;
 
     //Cached Variable
@@ -45,6 +44,7 @@ public class Block : MonoBehaviour
     private void HandleHit()
     {
         timesHit++;
+        int maxHits = hitSprites.Length + 1;
         if (maxHits <= timesHit)
         {
             DestroyBlock();
@@ -59,7 +59,14 @@ public class Block : MonoBehaviour
     private void ShowNextHitSprite()
     {
         int spriteIndex = timesHit - 1;
-        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        if (hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block Sprite is missing from array" + gameObject.name);
+        }
     }
 
     private void DestroyBlock()
