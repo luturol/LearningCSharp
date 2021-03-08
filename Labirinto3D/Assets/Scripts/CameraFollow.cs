70afba5c92c20bb00bd146ef556d5502005ad1f6
Rafael Ahrons <rafael.ahrons@gmail.com>
2021-03-08 13:27:43 -0300

    Add labirinto em 3D

---
+++ Labirinto3D/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    private Vector3 distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = transform.position - player.transform.position;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+        
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + distance;
+    }
+}
---
+++ Labirinto3D/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Vector3 movement;        
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {        
+        rigidbody.MovePosition(rigidbody.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
