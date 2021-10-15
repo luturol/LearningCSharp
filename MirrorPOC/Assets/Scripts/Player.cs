3f3ed9f6d1dabbf45c025d81760c5a30020b8a06
Rafael Ahrons <rafael.ahrons@gmail.com>
2021-10-15 01:30:56 -0300

    Hello world multiplayer

---
+++ MirrorPOC/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Player : NetworkBehaviour
+{
+    private void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
+
+            transform.position = transform.position + movement;
+        }
+    }
+
+    private void Update() {
+        HandleMovement();
+    }
+}
