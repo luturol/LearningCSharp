4fe1b183dd4a40ec9a2e52df1b9b0c4354f97c12
Rafael Ahrons <rafael.ahrons@gmail.com>
2022-01-04 19:15:35 -0300

    Add poc input without blocking console

---
+++ Snake/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        private static ConsoleKeyInfo lastPressed;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var readKeyTask = new Task(ReadKey);
+            var countNumbersTaks = new Task(CountNumbers);
+
+            countNumbersTaks.Start();
+            readKeyTask.Start();
+
+            Task.WaitAll(new Task[] { countNumbersTaks, readKeyTask });
+        }
+
+        private static void CountNumbers()
+        {
+            int i = 0;
+            while (true)
+            {
+                i++;                
+                Console.WriteLine(i.ToString());
+
+                if(lastPressed != default)
+                {
+                    Console.WriteLine("Ultima tecla pressionada: " + lastPressed.KeyChar);
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        private static void ReadKey()
+        {
+            ConsoleKeyInfo key = new ConsoleKeyInfo();
+
+            while (!Console.KeyAvailable && key.Key != ConsoleKey.Escape)
+            {
+                key = Console.ReadKey(true);
+                lastPressed = key;
+                Console.WriteLine("Tecla pressionada: " + key.KeyChar);
+            }
+        }
+    }
+}
