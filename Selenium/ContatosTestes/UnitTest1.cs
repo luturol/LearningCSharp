61edaad5fb7b9940af664d2b142035954e7ca989
Rafael Ahrons <rafael.ahrons@gmail.com>
2022-01-11 13:53:13 -0300

    Add Faker and project references

--- Selenium/ContatosTestes/UnitTest1.cs
+++ Selenium/ContatosTestes/UnitTest1.cs
@@ -8,7 +8,19 @@ namespace ContatosTestes
         [Fact]
         public void Test1()
         {
+            var nome = Faker.Name.FullName();            
+            var firstName = Faker.Name.First();
+            var email = Faker.Internet.Email();
+            var telefone = Faker.Phone.Number();
+            var empresa = Faker.Company.Name();
+            var endereco = Faker.Address.StreetName();
 
+            Console.WriteLine($"Full Name: { nome }");
+            Console.WriteLine($"First Name: { firstName }");
+            Console.WriteLine($"Email: { email }");
+            Console.WriteLine($"Telefone: { telefone }");
+            Console.WriteLine($"Empresa: { empresa }");
+            Console.WriteLine($"Endereco: { endereco }");
         }
     }
 }
