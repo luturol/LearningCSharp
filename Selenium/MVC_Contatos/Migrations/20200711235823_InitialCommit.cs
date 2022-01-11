4c24e62c0b1da655ba37cd0348c2edaf1eb78ce2
Rafael Ahrons <rafael.ahrons@gmail.com>
2022-01-11 13:19:01 -0300

    Add Contatos MVC e Contatos Test

---
+++ Selenium/MVC_Contatos/Migrations/20200711235823_InitialCommit.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVC_Contatos.Migrations
+{
+    public partial class InitialCommit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contatos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(maxLength: 100, nullable: false),
+                    Endereco = table.Column<string>(nullable: true),
+                    Cidade = table.Column<string>(nullable: true),
+                    Estado = table.Column<string>(nullable: true),
+                    Cep = table.Column<string>(nullable: true),
+                    Fone = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: false),
+                    EstadoCivil = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contatos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contatos");
+        }
+    }
+}
