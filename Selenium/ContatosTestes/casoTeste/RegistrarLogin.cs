13ef028251af8cfc1e559e77d6fcc2d1675c9ab8
Rafael Ahrons <rafael.ahrons@gmail.com>
2022-01-26 20:22:09 -0300

    Add registrar login

---
+++ Selenium/ContatosTestes/casoTeste/RegistrarLogin.cs
@@ -0,0 +1,47 @@
+using ContatosTestes.classesBase;
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using Selenium;
+
+namespace ContatosTestes.casoTeste
+{
+    public class RegistrarLogin : BaseClass
+    {
+        public RegistrarLogin(IConfiguration configuration, Browser browser) : base(configuration, browser)
+        {
+
+        }
+
+        public string RegistrarUsuario(string email, string senha)
+        {
+            var by = By.Name("RegistrarId");
+            webDriver.Wait(by);
+            webDriver.ClickButton(by);
+
+            webDriver.Wait(By.XPath("/html/body/div/main/div/div/form/h4"));
+
+            webDriver.SetValue(By.Id("Input_Email"), email);
+            webDriver.SetValue(By.Id("Input_Password"), senha);
+            webDriver.SetValue(By.Id("Input_ConfirmPassword"), senha);
+
+            webDriver.SubmitButton(By.ClassName("btn"));
+
+            webDriver.Wait(By.Id("confirm-link"));
+            webDriver.ClickButton(By.Id("confirm-link"));
+
+            webDriver.Wait(By.LinkText("Logar"));
+            webDriver.ClickButton(By.LinkText("Logar"));
+
+            webDriver.Wait(By.Id("Input_Email"));
+            webDriver.SetValue(By.Id("Input_Email"), email);
+            webDriver.SetValue(By.Id("Input_Password"), senha);
+            webDriver.ClickButton(By.Id("Input_RememberMe"));
+
+            webDriver.SubmitButton(By.ClassName("btn"));
+
+            webDriver.Wait(By.Id("UserIdentityEmailId"));
+            
+            return string.Empty;
+        }
+    }
+}
